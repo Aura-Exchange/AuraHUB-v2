06c7cf79ef7936b3f2a2fb78689dd02d6b1b52e0
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2023-05-19 22:07:37 -0500

    Update mint.tsx

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -47,7 +47,7 @@ const Mint: NextPage = () => {
                     Binance Smart Chain
                 </Text>
                 <iframe
-                    src="https://ipfs.thirdwebcdn.com/ipfs/QmfK9mw9eQKE9vCbtZht9kygpkNWffdwibsJPnCo7MBN4M/erc1155.html?contract=0x83fc7f12b5CEafEd4c435A26ec1758EdA4A4d267&chain=%7B%22name%22%3A%22Binance+Smart+Chain+Mainnet%22%2C%22chain%22%3A%22BSC%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fbinance.rpc.thirdweb.com%2F5a9bc94b87f7cbbbfbbc234bf1e07f0adf5f3cf3012c9f26f9fc9820d64df93a%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Binance+Chain+Native+Token%22%2C%22symbol%22%3A%22BNB%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22bnb%22%2C%22chainId%22%3A56%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22binance%22%7D&tokenId=0&theme=dark&primaryColor=yellow"
+                    src="https://ipfs-2.thirdwebcdn.com/ipfs/QmfK9mw9eQKE9vCbtZht9kygpkNWffdwibsJPnCo7MBN4M/erc1155.html?contract=0x83fc7f12b5CEafEd4c435A26ec1758EdA4A4d267&chain=%7B%22name%22%3A%22Binance+Smart+Chain+Mainnet%22%2C%22chain%22%3A%22BSC%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fbinance.rpc.thirdweb.com%2F5a9bc94b87f7cbbbfbbc234bf1e07f0adf5f3cf3012c9f26f9fc9820d64df93a%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Binance+Chain+Native+Token%22%2C%22symbol%22%3A%22BNB%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22bnb%22%2C%22chainId%22%3A56%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22binance%22%7D&tokenId=1&theme=dark&primaryColor=yellow"
                     width="600px"
                     height="600px"
                 ></iframe>
