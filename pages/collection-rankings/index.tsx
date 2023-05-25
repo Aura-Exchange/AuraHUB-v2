4dd9395b72d4ed6f3ab4421baa1811869d1903f4
NFTwolfmaximus <wolfmaximus.live@gmail.com>
2023-05-24 19:53:15 -0500

    Adjusting name for Firebase API key

--- pages/collection-rankings/index.tsx
+++ pages/collection-rankings/index.tsx
@@ -136,7 +136,7 @@ const IndexPage: NextPage<Props> = ({ ssr }) => {
 
   const getProjectList = async () => {
     const firebaseConfig = {
-    apiKey: process.env.API_KEY,
+    apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.FIREBASE_DATABASE_URL,
     projectId:process.env.FIREBASE_PROJECT_ID,
